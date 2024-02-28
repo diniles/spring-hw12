4630ffa1d9a500fcbabd80e7094b0b16ee116109
Dzmitry Liashko <dzliashko@gmail.com>
2024-02-28 13:58:49 +0300

    Feat: repository

---
+++ src/main/java/ru/gb/hw12/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package ru.gb.hw12.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.gb.hw12.entity.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+}
